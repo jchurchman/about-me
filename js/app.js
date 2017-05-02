e0c90deb62613f5b9021198e97a6d784f21584e6
Joe Churchman <churchman.joe@gmail.com>
2017-05-02 14:28:47 -0700

    Fixed JS page link from HTML, got JS started

---
+++ js/app.js
@@ -0,0 +1,5 @@
+"use strict";
+var userName = prompt( "Hello, what is your name?" );
+console.log( userName + " has logged on to the site.");
+var intro = prompt( "Hello " + userName + "! I bet you're here to learn about Joe. Would you like to play a quick guessing game to learn some neat stuff about him?" );
+console.log( userName + " has said that " + intro + ", they want to play.");
